4f8e8e298a58e0bc20d5b14b0ded96523cceb386
Sakik Al Sajid <sakikal.sajid@northsouth.edu>
2021-09-02 23:44:18 +0600

    setup routing and fundraising and membership design

--- src/index.js
+++ src/index.js
@@ -1,12 +1,69 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppContainer from './containers/app.container'
-import { BrowserRouter as Router } from 'react-router-dom'
 import './index.less'
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home/Home '
+import NavigationBar from './components/Shared/NavigationBar/NavigationBar'
+import SignUp from './components/SignUp/SignUp'
+import Login from './components/Login/Login'
+import UpdateProfile from './components/UpdateProfile/UpdateProfile'
+import MemberDirectory from './components/MemberDirectory/MemberDirectory'
+import EventsCalender from './components/EventsCalender/EventsCalender'
+import CareerOppurtunities from './components/CareerOppurtunities/CareerOppurtunities'
+import AlumniStories from './components/AlumniStories/AlumniStories'
+import FundRaising from './components/FundRaising/FundRaising'
+import NewsAndPhotos from './components/NewsAndPhotos/NewsAndPhotos'
+
 ReactDOM.render(
-  <Router>
-    <AppContainer />
-  </Router>,
+  <>
+    <NavigationBar />
+    <Router>
+      <AppContainer />
+
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route path='/home'>
+          <Home />
+        </Route>
+
+        <Route path='/signup'>
+          <SignUp />
+        </Route>
+
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/updateprofile'>
+          <UpdateProfile />
+        </Route>
+        <Route path='/MemberDirectory'>
+          <MemberDirectory />
+        </Route>
+        <Route path='/events'>
+          <EventsCalender />
+        </Route>
+        <Route path='/career'>
+          <CareerOppurtunities />
+        </Route>
+        <Route path='/stories'>
+          <AlumniStories />
+        </Route>
+        <Route path='/fundraising'>
+          <FundRaising />
+        </Route>
+        <Route path='/NewsAndPhotos'>
+          <NewsAndPhotos />
+        </Route>
+
+        <Route path='*'>nai</Route>
+      </Switch>
+    </Router>
+  </>,
   document.getElementById('root')
 )
